34e8d54f9bbf4552a5c48ae3e77702bdb68a085c
Cole Phillips <nphillips78@gmail.com>
2018-10-17 13:03:14 -0400

    logic

--- days_until/src/index.js
+++ days_until/src/index.js
@@ -4,9 +4,106 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+export default class Display extends React.Component {
+  constructor() {
+    super()
 
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+    }
+  }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  componentDidMount() {
+    this.interval = setInterval() => {
+      const date = this.calculateCountdown(this.props.date)
+      date ? this.setState(date) : this.stop()
+    }, 60000)
+  }
+
+  componentWillUnmount() {
+    this.stop()
+  }
+
+  calculateCountdown(endDate) {
+    let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) /60000
+
+    if (diff <= 0 return false)
+
+    const remaining = {
+      years: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0
+    }
+
+    if (diff >= (365.25 * 1440)) {
+      remaining.years = Math.floor(diff / (365.25 * 1440))
+      diff -= remaining.years * 365.25 * 1440
+    }
+    if (diff >= 1440) {
+      remaining.days = Math.floor( diff / 1440)
+      diff -= remaining.days * 1440
+    }
+    if (diff >= 60) {
+      remaining.min = Math.floor(diff / 60)
+      diff -= remaiing.min * 60
+    }
+
+    return remaining
+  }
+
+  stop() {
+    clearInterval(this.interval)
+  }
+
+  addLeadingZeros(value) {
+    value = String(value)
+    while (value.length < 2) {
+      value = '0' + value
+    }
+    
+    return value
+  }
+
+  render () {
+    const countDown = this.state
+
+    return (
+      <div className="Days">
+      <span className="Days - col">
+      <span className="Days-col-element">
+      <strong>{this.addLeadingZeros(countDown.days)}</strong>
+      <span>{countDown.days === 1 ? 'Day' : 'Days'}</span>
+        </span>
+      </span>
+      
+       <span className="Countdown-col">
+          <span className="Countdown-col-element">
+            <strong>{this.addLeadingZeros(countDown.hours)}</strong>
+            <span>Hours</span>
+          </span>
+        </span>
+
+         <span className="Countdown-col">
+          <span className="Countdown-col-element">
+            <strong>{this.addLeadingZeros(countDown.min)}</strong>
+            <span>Min</span>
+          </span>
+        </span>
+      </div>
+    )
+  }
+}
+
+Countdown.propTypes = {
+  date: PropTypes.string.isRequired
+}
+
+Countdown.defaultProps = {
+  date: new Date()
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
