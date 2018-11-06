fef65fa3ebaea634ef71075a160fc297ec535da4
Cole Phillips <nphillips78@gmail.com>
2018-11-06 16:07:56 -0500

    routes and views

--- days_until/src/App.js
+++ days_until/src/App.js
@@ -1,47 +1,13 @@
 import React from 'react';
-import DaysLeft from './countdown.js'
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+const App = () => {
+  return ( 
+  <BrowserRouter>
+    <Route path="/login" component = {Login}></Route>
+    <Route path="/CountdownView" component = {}
+  </BrowserRouter> 
+  );
+  }
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      input: ""
-    }
-  }
-  changeTitle = (e) => {
-    const target = e.target
-    const value = target.value
-    this.setState({title: value})
-  }
-  handleChange = (e) => {
-    const target = e.target
-    const value = target.value
-    this.setState({text: value})  
-  }
-  render() {
-    const currentDate = new Date()
-    const year = (currentDate.getMonth() && currentDate.getDate) ? currentDate.getFullYear()  : currentDate.getFullYear()
-    console.log("year is " + year)
-    console.log("month is " + currentDate.getMonth())
-    console.log("today is " + currentDate.getDate())
-
-    return (
-      <div className = "App">
-        <div className="App-header">
-          <img src={require('./12a1.svg')} className="App-logo" alt="calendar"/>
-          <h2> How many days until . . .</h2>
-        </div>
-      <div className="input">  
-      <input value ={this.state.title} placeholder="Name the day!" onChange={this.changeTitle} type ="title"></input>
-      <input value ={this.state.text} placeholder="Month and Day" onChange ={this.handleChange} type ="text"></input>
-      </div>
-    
-      <h3 className="tagline">{this.state.title} is on its way!</h3>
-      <DaysLeft date={`${year}-${this.state.text}T00:00:00`} />
-      </div>
-    )
-  }
-}
+export default App;
---
+++ days_until/src/login.js
@@ -0,0 +1,1 @@
+LOGIN shit goes here 
