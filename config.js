b201ae450c399e7feded4568c11df9c20b81e2aa
FJF616 <franzjfernando@hotmail.com>
2018-11-14 13:53:45 -0800

    added notes to config.js file

--- config.js
+++ config.js
@@ -2,3 +2,30 @@ const mysecret = '1234 I declare thumb war';
 module.exports = {
   mysecret
 };
+
+
+// notes 
+
+//front end axios call
+
+const allEntries = async () => {
+  const entries = await axios.get('http://localhost:5000/entries').data;
+    this.setState({ entries });
+}
+
+
+const allEntries = async () => {
+  const { match: { params } } = this.props;
+  const entry = await axios.get(`http://localhost:5000/${params.entryId}`).data;
+    this.setState({ entry });
+}
+
+
+
+//need these routes on frontend or something similar
+<Route exact path='/' component={Entries}/>                        //this is very similar to a todolist, where you must have a couple react components that will render out a list
+<Route exact path='/entry/:entryId' component={Entry}/>            //the Entries component will render the Entry as a child component
+
+
+
+
